b5cf02c56cdcb7c10057d752d3e120fd44b3bb9d
minjunsong <songmj5000@naver.com>
2024-07-08 12:53:18 +0900

    스프링부트 공부

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hello_spring.controller;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 응답 바디 부분에 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name; // "hello spring" 데이터를 그대로 보냄
+    }
+
+    @GetMapping("hello-api") // 객체 반환 json
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
